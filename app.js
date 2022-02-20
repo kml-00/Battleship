2a72db62d54cc9836c63415b047805845c262193
kml@kmldev.com <kamillo57@wp.pl>
2022-02-20 09:23:18 +0000

    event listeners   functions  added

--- app.js
+++ app.js
@@ -225,6 +225,101 @@ const rotate = () => {
 
 /* ship rotate function */
 
+/* event listeners function */
+
+const addCustomListeners = () => {
+  let ShipDropArea = getShipDropArea(currentShip.id);
+  let direction = getShipDirection(currentShip.id);
+  let positionOnBoard = getShipPositionOnBoardAll();
+  switch (direction) {
+    case "horizontal":
+      for (let i = 0; i < game.length; i++) {
+        for (let j = 0; j < game.length - ShipDropArea; j++) {
+          let element = document.getElementById(game[i][j]);
+          element.style.backgroundColor = "#ABA2EF";
+          element.addEventListener("drop", drop);
+          element.addEventListener("dragover", allowDrop);
+        }
+      }
+
+      break;
+    case "vertical":
+      for (let i = 0; i < game.length - ShipDropArea; i++) {
+        for (let j = 0; j < game.length; j++) {
+          let element = document.getElementById(game[i][j]);
+          element.style.backgroundColor = "#ABA2EF";
+          element.addEventListener("drop", drop);
+          element.addEventListener("dragover", allowDrop);
+        }
+      }
+
+      break;
+  }
+  getPositionOnBoard();
+  positionOnBoard.forEach((id) => {
+    let element = document.getElementById(id);
+
+    element.style.backgroundColor = "#fff";
+    element.removeEventListener("drop", drop);
+    element.removeEventListener("dragover", allowDrop);
+  });
+};
+const removeAllListeners = () => {
+  for (let i = 0; i < game.length; i++) {
+    for (let j = 0; j < game.length; j++) {
+      let element = document.getElementById(game[i][j]);
+      element.style.backgroundColor = "#fff";
+      element.removeEventListener("drop", drop);
+      element.removeEventListener("dragover", allowDrop);
+    }
+  }
+};
+
+const elementPosition = () => {
+  let element = null;
+  ships.forEach((ship) => {
+    if (ship.onGameboard) {
+      if (ship.id !== currentShip.id) {
+        let left = ship.dropPoint % 10;
+        let top = Math.floor(ship.dropPoint / 10);
+        let position = ship.positionOnBoard;
+        let direction = getShipDirection(currentShip.id);
+
+        let dropSize = getShipDropArea(currentShip.id);
+        if (direction == "horizontal") {
+          for (let i = 1; i < dropSize + 1; i++) {
+            position.forEach((point) => {
+              let id = parseInt(point) - i;
+
+              element = document.getElementById(id);
+
+              element.style.backgroundColor = "#fff";
+              element.removeEventListener("drop", drop);
+              element.removeEventListener("dragover", allowDrop);
+            });
+          }
+        }
+        if (direction == "vertical") {
+          for (let i = 1; i < dropSize + 1; i++) {
+            let id = parseInt(ship.dropPoint) - i * 10;
+            position.forEach((point) => {
+              let id = parseInt(point) - i * 10;
+
+              element = document.getElementById(id);
+
+              element.style.backgroundColor = "#fff";
+              element.removeEventListener("drop", drop);
+              element.removeEventListener("dragover", allowDrop);
+            });
+          }
+        }
+      }
+    }
+  });
+};
+
+/* event listeners function */
+
 /* Init function  */
 const init = () => {
   let gameheader = document.getElementById("header");
